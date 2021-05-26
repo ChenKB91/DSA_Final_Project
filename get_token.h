06bfbc56d0f0ee8250dd90b387da865b6a78f450
hyder308116 <b09902100@csie.ntu.edu.tw>
2021-05-26 15:35:11 +0800

    fix bugs in get_token.h

--- get_token.h
+++ get_token.h
@@ -11,6 +11,7 @@ void get_token(str2token* st, char* s) {
     int len = strlen(s);
     int last = -1; // last position of [0-9a-zA-Z] alphabet "-1" means the previous alphabet is a non [0-9a-zA-Z] alphabet
     st->token = malloc(len*sizeof(char*)/2);
+    st->sz = 0;
     for(int i = 0; i <= len; i++) {
         if(isdigit(s[i]) || isalpha(s[i])) {
             if(last == -1)

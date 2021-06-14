70c5a9d8ae1a7afe7a821bbf4159b6b9591bbdd1
Frank Kuo <frankkuo@protonmail.com>
2021-06-14 12:16:36 +0800

    modify hash table to use prefix

--- hash_table.c
+++ hash_table.c
@@ -4,18 +4,30 @@
 
 #define MAGIC_MUL 31
 #define KEY_RANGE 1000000007
-#define KEY_SIZE 10000
+#define KEY_SIZE 10005
+#define MAIL_N 10005
+
+const int transfer[128] = { // 16 per line
+    -1, -1, -1, -1, -1, -1, -1, -1,  -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1,  -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1,  -1, -1, -1, -1, -1, -1, -1, -1,
+     0,  2,  1,  0,  0,  0,  0,  0,   0,  0, -1, -1, -1, -1, -1, -1,
+    -1, 14,  8, 10,  3,  2,  6,  1,   5, 11,  0,  0,  3,  4,  1, 12,
+     7,  0,  4, 13, -1,  1,  0,  9,   0,  0,  0, -1, -1, -1, -1, -1,
+    -1, 14,  8, 10,  3,  2,  6,  1,   5, 11,  0,  0,  3,  4,  1, 12,
+     7,  0,  4, 13, -1,  1,  0,  9,   0,  0,  0, -1, -1, -1, -1, -1
+};
 
 typedef struct __node__ {
     char key[KEY_SIZE];
-    int val;
+    int mail_cnt;
+    int mails[MAIL_N];
     struct __node__* nxt;
 } node;
 
 typedef struct __llist__ {
     node* head;
 } llist;
-
 
 int hashStr(char* s) {
     long long int hash = 0;
@@ -30,22 +42,47 @@ llist* newDict() {
     return dict;
 }
 
-void addEntry(llist* dict, char* key, int val) {
+llist** initTable() {
+    llist** table = malloc(15*sizeof(llist*));
+    for (int i=0; i<15; i++) {
+        table[i] = calloc(KEY_RANGE, sizeof(llist));
+    }
+    return table;
+}
+
+
+void newKey(llist** table, char* key, int mail_id) {
+    llist* dict = table[transfer[key[0]]];
     node* new = malloc(sizeof(node));
     int idx = hashStr(key);
     strcpy(new->key, key);
-    new->val = val;
+    new->mail_cnt = 1;
+    new->mails[0] = mail_id;
     new->nxt = dict[idx].head;
     dict[idx].head = new;
 }
 
-int lookup(llist* dict, char* key) {
-    // return -1 if not in dictionary
+int* getMails(llist** table, char* key) {
+    llist* dict = table[transfer[key[0]]];
     int idx = hashStr(key);
     node* cur_node = dict[idx].head;
     while (cur_node) {
         if (strcmp(key, cur_node->key) == 0) {
-            return cur_node->val;
+            return cur_node->mails;
+        } else {
+            cur_node = cur_node->nxt;
+        }
+    }
+    return NULL;
+}
+
+int getMailCnt(llist** table, char* key) {
+    llist* dict = table[transfer[key[0]]];
+    int idx = hashStr(key);
+    node* cur_node = dict[idx].head;
+    while (cur_node) {
+        if (strcmp(key, cur_node->key) == 0) {
+            return cur_node->mail_cnt;
         } else {
             cur_node = cur_node->nxt;
         }
@@ -53,22 +90,21 @@ int lookup(llist* dict, char* key) {
     return -1;
 }
 
-
 int main() {
     char k1[100] = "Hello!"; int v1 = 41;
     char k2[100] = "Hi!";    int v2 = 37;
     char k3[100] = "Howdy!"; int v3 = 29;
     char k4[100] = "Hola!";  int v4 = 13;
     char k5[100] = "Aloha!";
-    llist* dict = newDict();
-    addEntry(dict, k1, v1);
-    addEntry(dict, k2, v2);
-    addEntry(dict, k3, v3);
-    addEntry(dict, k4, v4);
-    printf("%d\n", lookup(dict, k1));
-    printf("%d\n", lookup(dict, k2));
-    printf("%d\n", lookup(dict, k3));
-    printf("%d\n", lookup(dict, k4));
-    printf("%d\n", lookup(dict, k5));
+    llist** table = initTable();
+    newKey(table, k1, v1);
+    newKey(table, k2, v2);
+    newKey(table, k3, v3);
+    newKey(table, k4, v4);
+    printf("%d\n", getMailCnt(table, k1));
+    printf("%d\n", getMailCnt(table, k2));
+    printf("%d\n", getMailCnt(table, k3));
+    printf("%d\n", getMailCnt(table, k4));
+    printf("%d\n", getMailCnt(table, k5));
     return 0;
 }

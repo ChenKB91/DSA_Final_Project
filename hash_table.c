ed14c94845381d1a584f71ec44d4199dc8112645
Frank Kuo <frankkuo@protonmail.com>
2021-06-16 17:52:38 +0800

    add some comments

--- hash_table.c
+++ hash_table.c
@@ -11,6 +11,7 @@
 Prime number source: https://primes.utm.edu/lists/small/100000.txt
 */
 
+// Get index of the hash table by char
 const int transfer[128] = { // 16 per line
     -1, -1, -1, -1, -1, -1, -1, -1,  -1, -1, -1, -1, -1, -1, -1, -1,
     -1, -1, -1, -1, -1, -1, -1, -1,  -1, -1, -1, -1, -1, -1, -1, -1,
@@ -22,8 +23,8 @@ const int transfer[128] = { // 16 per line
      7,  0,  4, 13, -1,  1,  0,  9,   0,  0,  0, -1, -1, -1, -1, -1
 };
 
+// Count collision times during addMailToToken()
 int COLLISION_CNT = 0;
-
 
 int hashStr(char* s) {
     long long int hash = 0;
@@ -33,6 +34,7 @@ int hashStr(char* s) {
     return hash;
 }
 
+// Linked list for storing mail ids
 typedef struct __mnode__ {
     int mail_id;
     struct __mnode__* nxt;
@@ -55,11 +57,12 @@ void mlistInsert(mlist* l, int mail_id) {
     l->head = new;
 }
 
+// Hash table by chaining, use tokens as index
 typedef struct __node__ {
     char key[KEY_SIZE];
-    int mail_cnt;
-    int mail_arr[MAIL_N];
-    mlist* mail_list;
+    int mail_cnt; // Number of mails with this token
+    int mail_arr[MAIL_N]; // Marking if a mail have this token
+    mlist* mail_list; // All mails with this token (in linked list)
     struct __node__* nxt;
 } node;
 
@@ -68,6 +71,8 @@ typedef struct __llist__ {
 } llist;
 
 llist* newDict() {
+    // Returns a dictionary / hash table
+    // There seems to be some problem using calloc (Not Sure)
     // llist* dict = calloc(KEY_RANGE, sizeof(llist));
     llist* dict = malloc(KEY_RANGE*sizeof(llist));
     for (int i=0; i<KEY_RANGE; i++) {
@@ -77,6 +82,7 @@ llist* newDict() {
 }
 
 llist** initTable() {
+    // Returns a table, each element is a hash table
     llist** table = malloc(15*sizeof(llist*));
     for (int i=0; i<15; i++) {
         table[i] = newDict();
@@ -85,6 +91,7 @@ llist** initTable() {
 }
 
 void newKey(llist** table, char* key, int mail_id) {
+    // Add new key
     llist* dict = table[transfer[key[0]]];
     node* new = malloc(sizeof(node));
     int idx = hashStr(key);
@@ -99,6 +106,8 @@ void newKey(llist** table, char* key, int mail_id) {
 }
 
 void addMailToToken(llist** table, char* key, int mail_id) {
+    // Add a mail_id by token
+    // Creates a new key if doesn't exist
     llist* dict = table[transfer[key[0]]];
     int idx = hashStr(key);
     node* cur_node = dict[idx].head;
@@ -119,6 +128,7 @@ void addMailToToken(llist** table, char* key, int mail_id) {
 }
 
 mlist* getMails(llist** table, char* key) {
+    // Get linked list of mails
     llist* dict = table[transfer[key[0]]];
     int idx = hashStr(key);
     node* cur_node = dict[idx].head;
@@ -132,7 +142,23 @@ mlist* getMails(llist** table, char* key) {
     return NULL;
 }
 
+int* getMailArr(llist** table, char* key) {
+    // Get array of mails
+    llist* dict = table[transfer[key[0]]];
+    int idx = hashStr(key);
+    node* cur_node = dict[idx].head;
+    while (cur_node) {
+        if (strcmp(key, cur_node->key) == 0) {
+            return cur_node->mail_arr;
+        } else {
+            cur_node = cur_node->nxt;
+        }
+    }
+    return NULL;
+}
+
 int getMailCnt(llist** table, char* key) {
+    // Get number of mails
     llist* dict = table[transfer[key[0]]];
     int idx = hashStr(key);
     node* cur_node = dict[idx].head;
@@ -147,6 +173,7 @@ int getMailCnt(llist** table, char* key) {
 }
 
 int main() {
+    // For testing, k5 is intented to be a key not inserted
     char k1[100] = "Hello!"; int v1 = 41;
     char k2[100] = "Hi!";    int v2 = 37;
     char k3[100] = "Howdy!"; int v3 = 29;
@@ -158,10 +185,16 @@ int main() {
     addMailToToken(table, k2, v2);
     addMailToToken(table, k3, v3);
     addMailToToken(table, k4, v4);
+    printf("getMailCnt():\n");
     printf("%d\n", getMailCnt(table, k1));
     printf("%d\n", getMailCnt(table, k2));
     printf("%d\n", getMailCnt(table, k3));
     printf("%d\n", getMailCnt(table, k4));
     printf("%d\n", getMailCnt(table, k5));
+    printf("getMailArr():\n");
+    printf("%d\n", getMailArr(table, k1)[v1]);
+    printf("%d\n", getMailArr(table, k1)[v2]);
+    printf("%d\n", getMailArr(table, k1)[v3]);
+    printf("%d\n", getMailArr(table, k1)[v4]);
     return 0;
 }

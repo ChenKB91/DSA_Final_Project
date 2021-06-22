b4428b1d44bdf03f08d9553843c6e1ba174cad71
hyder308116 <b09902100@csie.ntu.edu.tw>
2021-06-22 21:44:32 +0800

    update str2token structure, and add function to get token from subject

--- get_token.h
+++ get_token.h
@@ -1,47 +1,49 @@
-// Code by hyder308116
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <ctype.h>
 #include <stdbool.h>
 typedef struct {
-    int sz;
+    int sub_sz, sz;
+    char **sub_token;
     char **token;
 } str2token;
-void get_token(str2token* st, char* s) {
-    int len = strlen(s);
+void get_token(str2token* st, mail* s) {
+    int len = strlen(s->content);
     int last = -1; // last position of [0-9a-zA-Z] alphabet "-1" means the previous alphabet is a non [0-9a-zA-Z] alphabet
     st->token = malloc(len*sizeof(char*)/2);
     st->sz = 0;
-    for(int i = 0; i <= len; i++) {
-        if(isdigit(s[i]) || isalpha(s[i])) {
-            if(last == -1)
+    for (int i = 0; i <= len; i++) {
+        if (isdigit(s->content[i]) || isalpha(s->content[i])) {
+            if (last == -1)
                 last = i;
         }
-        else if(last != -1) {
+        else if (last != -1) {
             st->token[st->sz] = malloc((i-last+1)*sizeof(char));
-            strncpy(st->token[st->sz], s+last, i-last);
+            strncpy(st->token[st->sz], s->content+last, i-last);
             st->token[st->sz][i-last] = '\0';
             last = -1;
             st->sz++;
         }
     }
-    st->token = realloc(st->token,st->sz*sizeof(char*));
+    st->token = realloc(st->token, st->sz*sizeof(char*));
+    len = strlen(s->subject);
+    last = -1;
+    st->sub_token = malloc(len*sizeof(char)/2);
+    st->sub_sz = 0;
+    for (int i = 0; i <= len; i++) {
+        if (isdigit(s->subject[i]) || isalpha(s->subject[i])) {
+            if(last == -1)
+                last = i;
+        }
+        else if (last != -1) {
+            st->sub_token[st->sub_sz] = malloc((i-last+1)*sizeof(char));
+            strncpy(st->sub_token[st->sub_sz], s->subject+last, i-last);
+            st->sub_token[st->sub_sz][i-last] = '\0';
+            last = -1;
+            st->sub_sz++;
+        }
+    }
+    st->sub_token = realloc(st->sub_token, st->sub_sz*sizeof(char));
     return;
-}
-
-str2token* readTokenFromFile(char* filename){
-    FILE* fp;
-    fp = fopen(filename, "r");
-    long size;
-    fseek(fp, 0, SEEK_END); 
-    size = ftell(fp);
-    fseek(fp, 0, SEEK_SET); 
-    char* buf = malloc(size);
-    fread(buf, 1, size, fp);
-    
-    str2token* st = malloc(sizeof(str2token));
-    get_token(st, buf);
-    fclose(fp);
-    return st;
 }

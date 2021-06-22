f58351b7582ad7021bc52ce5bca487c2018de910
hyder308116 <b09902100@csie.ntu.edu.tw>
2021-06-22 11:52:07 +0800

    group_analyse.h update and fix bug

--- group_analyse.h
+++ group_analyse.h
@@ -1,14 +1,13 @@
-// Code by hyder308116
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <stdbool.h>
+const int name_hash_mod = 20118;
 typedef struct {
-    char _name[20];
     int parent, sz;
 } dsu_unit;
-dsu_unit dsu[1000007];
-int dsu_fill[1000007] = {0};
+dsu_unit dsu[20118];
+int dsu_fill[20118];
 void swap(int *a, int *b) {
     int _ = *a;
     *a = *b;
@@ -16,51 +15,48 @@ void swap(int *a, int *b) {
 }
 int str_hash(char *a) {
     int ret = 0, now = 0;
-    while(a[now]) {
-        ret = (ret * 31 + a[now]) % 1000007;
+    while (a[now]) {
+        ret = (ret * 31 + a[now]) % name_hash_mod;
         now++;
     }
     return ret;
 }
-void dsu_init(int n, char *usr) {
+void dsu_init(int n) {
     dsu[n].parent = n;
     dsu[n].sz = 1;
-    strcpy(usr, dsu[n]._name);
 }
 int find_set(int x) {
-    if (dsu[x].parent==x) return x;
+    if (dsu[x].parent == x) return x;
     dsu[x].parent = find_set(dsu[x].parent);
     return dsu[x].parent;
 }
 void set_union(int x, int y) {
     int set_x = find_set(x), set_y = find_set(y);
+    if(dsu[set_x].sz < dsu[set_y].sz)
+        swap(&set_x, &set_y);
     if (set_x != set_y) {
-        if (dsu[set_x].sz < dsu[set_y].sz)
-            swap(&set_x, &set_y);
         dsu[set_y].parent = set_x;
         dsu[set_x].sz += dsu[set_y].sz;
-        return dsu[set_x].sz;
     }
-    return dsu[set_x].sz > dsu[set_y].sz ? dsu[set_x].sz : dsu[set_y].sz;
+    return dsu[set_x].sz;
 }
 int* group_analyse(int n, char ***data) {
-    int gl[2] = {0}, ht[2]; // gl[0]:n_g, gl[1]:l_g
+    int gl[2] = {0}, ht[2];
     for (int i = 0; i < n; i++) {
         ht[0] = str_hash(data[i][0]);
         ht[1] = str_hash(data[i][1]);
         for (int j = 0; j < 2; j++) {
-            if (dsu_fill[ht[j]]) {
-                while (strcmp(data[i][j], dsu[ht[j]]._name))
-                    ht[j]++
-            }
             if (!dsu_fill[ht[j]]) {
-                dsu_init(ht[j], data[i][j]);
+                dsu_fill[ht[j]] = 1;
                 gl[0]++;
+                dsu_init(ht[j]);
             }
         }
-        if (find_set(dsu[ht[0]].parent) != find_set(dsu[ht[1]].parent)) gl[0]--;
-        int l = set_union(ht[0], ht[1]);
-        gl[1] = gl[1] > l ? gl[1] : l;
+        if (find_set(ht[0]) != find_set(ht[1])) {
+            gl[0]--;
+            int l = set_union(ht[0], ht[1]);
+            gl[1] = gl[1] > l ? gl[1] : l
+        }
     }
     return gl;
 }

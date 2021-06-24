7990f113cdfffbb0a7f1cc9b154d18e1dc484317
hyder308116 <b09902100@csie.ntu.edu.tw>
2021-06-24 22:31:23 +0800

    upload main_query3.c with bug

---
+++ main_query3.c
@@ -0,0 +1,89 @@
+// Code by hyder308116
+#include "api.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <stdbool.h>
+#define GROUP_MODL 20118
+#define POW_BASE 31
+
+int n_mails, n_queries;
+mail *mails;
+query *queries;
+FILE *outfile;
+
+typedef struct {
+    int parent, sz;
+} dsu_unit;
+dsu_unit dsu[GROUP_MODL];
+int dsu_fill[GROUP_MODL];
+void swap(int *a, int *b) {
+    int _ = *a;
+    *a = *b;
+    *b = _;
+}
+int str_hash(char *a) {
+    int ret = 0, now = 0;
+    while (a[now]) {
+        ret = (ret * POW_BASE + a[now]) % GROUP_MODL;
+        now++;
+    }
+    return ret;
+}
+void dsu_init(int n) {
+    dsu[n].parent = n;
+    dsu[n].sz = 1;
+}
+int find_set(int x) {
+    if (dsu[x].parent == x) return x;
+    dsu[x].parent = find_set(dsu[x].parent);
+    return dsu[x].parent;
+}
+int set_union(int x, int y) {
+    int set_x = find_set(x), set_y = find_set(y);
+    if (dsu[set_x].sz < dsu[set_y].sz) 
+        swap(&set_x, &set_y);
+    if (set_x != set_y) {
+        dsu[set_y].parent = set_x;
+        dsu[set_x].sz += dsu[set_y].sz;
+    }
+    return dsu[set_x].sz;
+}
+int* group_analyse_func(int n, mail* data, int *usr) {
+    int gl[2] = {0}, ht[2];
+    for (int i = 0; i < n; i++) {
+        ht[0] = str_hash(data[usr[i]].from);
+        ht[1] = str_hash(data[usr[i]].to);
+//        fprintf(outfile, "%s %s\n", data[usr[i]].from, data[usr[i]].to);    
+        for (int j = 0; j < 2; j++) {
+            if (!dsu_fill[ht[j]]) {
+                dsu_fill[ht[j]] = 1;
+                gl[0]++;
+                dsu_init(ht[j]);
+            }
+        }
+        if (find_set(ht[0]) != find_set(ht[1])) {
+            gl[0]--;
+            int l = set_union(ht[0], ht[1]);
+            gl[1] = gl[1] > l ? gl[1] : l;
+        }
+    }
+    return gl;
+}
+
+int main() {
+    api.init(&n_mails, &n_queries, &mails, &queries);
+    outfile = fopen("out.txt", "w");
+    for (int i = 0; i < n_queries; i++) {
+        if (queries[i].type == group_analyse) {
+            memset(dsu_fill, 0, sizeof(dsu_fill));
+            int len = queries[i].data.group_analyse_data.len;
+            int *mids = queries[i].data.group_analyse_data.mids;
+            int *gl = group_analyse_func(len, mails, mids);
+            fprintf(outfile, "%d: %d %d\n", queries[i].id, gl[0], gl[1]);
+            api.answer(queries[i].id, gl, 2);
+        }
+    }
+    return 0;
+}

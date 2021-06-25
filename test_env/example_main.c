99521e0829ec3b19dbd09e0b52a8202d838d7957
Frank Kuo <frankkuo@protonmail.com>
2021-06-25 15:00:52 +0800

    add MAIN ULTIMATE  !!!

--- test_env/example_main.c
+++ test_env/example_main.c
@@ -10,14 +10,17 @@ query *queries;
 
 int main(void){
 	api.init(&n_mails, &n_queries, &mails, &queries);
+	int cnt = 0;
 	double reward = 0;
 	for (int i = 0; i < n_queries; i++) {
-		if (queries[i].type == expression_match) {
+		if (queries[i].type == group_analyse) {
 			reward += queries[i].reward;
+			cnt++;
 		}
 		// 	printf("%d\n", i);
 		// 	// api.answer(queries[i].id, NULL, 0);
 	}
+	printf("%d\n", cnt);
 	printf("%f\n", reward);
 	return 0;
 }
